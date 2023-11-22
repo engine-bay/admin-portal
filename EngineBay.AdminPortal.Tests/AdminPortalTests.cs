4ad25ba7306290290f06208dcc013be0dc24bf2d
Janus Liebenberg <janus.liebenberg@entelect.co.uk>
2023-11-22 15:18:26 +0000

    add a single test

---
+++ EngineBay.AdminPortal.Tests/AdminPortalTests.cs
@@ -0,0 +1,13 @@
+namespace EngineBay.AdminPortal.Tests
+{
+    using Xunit;
+
+    public class AdminPortalTests
+    {
+        [Fact]
+        public void ControlTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
